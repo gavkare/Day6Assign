08f5df3e9ca96d09d80ff8fd493712cdbc8c52f1
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-24 11:48:56 +0530

    Coupon.java

---
+++ Coupon.java
@@ -0,0 +1,29 @@
+package Day6Assign;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Coupon {
+    public static int generateRandom() {
+        Random rand = new Random();
+        return rand.nextInt(1000);
+    }
+
+    public static int processCoupons(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+        while (set.size() < n) {
+            int random = generateRandom();
+            count++;
+            set.add(random);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int totalRandom = processCoupons(n);
+        System.out.println("Total random numbers needed: " + totalRandom);
+    }
+}
+
